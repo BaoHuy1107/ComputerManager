6add28c94bcfbfb6f5a8e062c5d1c85812ee4adb
BaoHuy1107 <baohuy98cva@gmail.com>
2024-08-29 16:06:48 +0700

    messageee

--- src/main/java/com/example/demoSpringBoot/controller/CustomerController.java
+++ src/main/java/com/example/demoSpringBoot/controller/CustomerController.java
@@ -65,7 +65,7 @@ public class CustomerController {
             return "updateCustomer";
         }
         this.customerService.saveCustomer(customer);
-        System.out.println(">>>");
+        System.out.println(">>>>");
         return "redirect:/select/customer";  // chuyển hướng qua url /select/customer
     }
 }
